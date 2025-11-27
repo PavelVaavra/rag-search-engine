0edd83498663c6e354c0ca66da37065a88ba4d24
PavelVaavra <pavelvavra.vm@gmail.com>
2025-11-27 17:07:57 +0100

    Punctuation

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
+import string
+
+def get_movies(keyword):
+    translator = str.maketrans("", "", string.punctuation)
+
+    keyword = keyword.lower().translate(translator)
+    keyword_tokens = [word for word in keyword if word != ""]
+    
+    with open("data/movies.json", "r") as file:
+        movies = json.load(file)["movies"]
+        found_movies = []
+
+        for movie in movies:
+            movie_modified = movie["title"].lower().translate(translator)
+            movie_tokens = [m for m in movie_modified if m != ""]
+            
+            # for movie_token in movie_tokens:
+            if keyword in movie_modified:
+                found_movies.append(movie["title"])
+                    # break
+            if len(found_movies) == 5:
+                break
+        
+        for i, movie in enumerate(found_movies):
+            print(f"{i + 1} {movie}")
 
 
 def main() -> None:
@@ -15,6 +41,7 @@ def main() -> None:
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
+            get_movies(args.query)
         case _:
             parser.print_help()
 
