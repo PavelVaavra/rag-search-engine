36a8e0be4761149c9a5ea54c7411941f6b009942
PavelVaavra <pavelvavra.vm@gmail.com>
2025-11-29 17:17:08 +0100

    Inverted Index

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.helpers import get_movies_by_keyword
+from lib.helpers import get_movies_by_keyword, build_idx
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -10,12 +10,17 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    subparsers.add_parser("build", help="Build inverted index from movies.json")
+
     args = parser.parse_args()
 
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
             get_movies_by_keyword(args.query)
+        case "build":
+            print("Building inverted index")
+            build_idx()
         case _:
             parser.print_help()
 
--- cli/lib/helpers.py
+++ cli/lib/helpers.py
@@ -42,3 +42,64 @@ def get_movies_by_keyword(keyword):
         
     for i, movie in enumerate(found_movies):
         print(f"{i + 1} {movie}")
+
+import pickle
+from pathlib import Path
+
+class InvertedIndex():
+    def __init__(self, path):
+        # path to json data
+        self.path = path
+        # a dictionary mapping tokens (strings) to sets of document IDs (integers).
+        self.index = {}
+        # a dictionary mapping document IDs to their full document objects.
+        self.docmap = {}
+
+    def __add_document(self, doc_id, text):
+        """tokenize the input text, then add each token to the index with the document ID"""
+        text_tokens = process_str(text)
+        for text_token in text_tokens:
+            try:
+                self.index[text_token.lower()].add(doc_id)
+            except KeyError:
+                self.index[text_token.lower()] = {doc_id}
+
+    def get_documents(self, term):
+        """get the set of document IDs for a given token, and return them as a list, sorted in ascending order"""
+        # lowercase term
+        try:
+            return sorted(self.index[term.lower()])
+        except IndexError:
+            return []
+
+    def build(self):
+        """iterate over all the movies from self.path and add them to both the index and the docmap"""
+        # When adding the movie data to the index with __add_document(), concatenate the title and the description and use that as the input text. 
+        # For example: f"{m['title']} {m['description']}"
+        with open(self.path, "r") as file:
+            movies = json.load(file)["movies"]
+
+        for movie in movies:
+            self.__add_document(movie["id"], f"{movie['title']} {movie['description']}")
+
+            self.docmap[movie["id"]] = movie
+
+    def save(self):
+        """save the index and docmap attributes to disk using the pickle module's dump function"""
+        # Use the file path/name cache/index.pkl for the index.
+        # Use the file path/name cache/docmap.pkl for the docmap.
+        # Have this method create the cache directory if it doesn't exist (before trying to write files into it).
+        Path("cache").mkdir(parents=True, exist_ok=True)
+
+        with open("cache/index.pkl", "wb") as file:
+            pickle.dump(self.index, file)
+
+        with open("cache/docmap.pkl", "wb") as file:
+            pickle.dump(self.docmap, file)
+
+def build_idx():
+    inverted_idx = InvertedIndex("data/movies.json")
+    inverted_idx.build()
+    inverted_idx.save()
+    docs = inverted_idx.get_documents("merida")
+    print(f"First document for token 'merida' = {docs[0]}")
