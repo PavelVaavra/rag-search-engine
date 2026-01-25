f0d38cdcd29b926af5473f70bb8ebaebb4a06b2f
PavelVaavra <pavelvavra.vm@gmail.com>
2026-01-25 07:46:15 +0100

    Multimodal Search Implementation

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -1,16 +1,47 @@
 from PIL import Image
 from sentence_transformers import SentenceTransformer
 
+from .search_utils import DATA_PATH, DEFAULT_SEARCH_LIMIT
+from .semantic_search import get_documents, cosine_similarity
+
 class MultimodalSearch:
-    def __init__(self, model_name="clip-ViT-B-32"):
+    def __init__(self, documents, model_name="clip-ViT-B-32"):
         self.model = SentenceTransformer(model_name)
+        self.documents = documents
+        
+        self.texts = []
+        for doc in self.documents:
+            self.texts.append(f"{doc["title"]}: {doc["description"]}")
+
+        self.text_embeddings = self.model.encode(self.texts, show_progress_bar=True)
 
     def embed_image(self, path):
         image = Image.open(path)
         return self.model.encode([image])[0]
     
+    def search_with_image(self, path):
+        img_embedding = self.embed_image(path)
+
+        similarity_results = []
+        for i, text_embedding in enumerate(self.text_embeddings):
+            similarity_results.append({
+                **self.documents[i], 
+                "similarity_score": cosine_similarity(text_embedding, img_embedding)
+            })
+
+        similarity_results.sort(key=lambda item: item["similarity_score"], reverse=True)
+
+        return similarity_results[:DEFAULT_SEARCH_LIMIT]
+    
 def verify_image_embedding(path):
-    multimodal_search = MultimodalSearch()
+    documents = get_documents(DATA_PATH)
+    multimodal_search = MultimodalSearch(documents)
     embedding = multimodal_search.embed_image(path)
 
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
+
+def image_search_command(path):
+    documents = get_documents(DATA_PATH)
+    multimodal_search = MultimodalSearch(documents)
+
+    return multimodal_search.search_with_image(path)
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -1,19 +1,32 @@
 import argparse
 
-from lib.multimodal_search import verify_image_embedding
+from lib.multimodal_search import verify_image_embedding, image_search_command
 
 def main():
     parser = argparse.ArgumentParser(description="Multimodal Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    summarize_parser = subparsers.add_parser("verify_image_embedding", help="Perform verification of an image embedding")
-    summarize_parser.add_argument("image_path", type=str, help="Path to an image to be embedded")
+    verify_image_embedding_parser = subparsers.add_parser("verify_image_embedding", help="Perform verification of an image embedding")
+    verify_image_embedding_parser.add_argument("image_path", type=str, help="Path to an image to be embedded")
+
+    image_search_parser = subparsers.add_parser("image_search", help="Search image through documents")
+    image_search_parser.add_argument("image_path", type=str, help="Path to an image to be searched for")
 
     args = parser.parse_args()
 
     match args.command:
         case "verify_image_embedding":
             verify_image_embedding(args.image_path)
+
+        case "image_search":
+            found_docs = image_search_command(args.image_path)
+
+            for i in range(len(found_docs)):
+                title = found_docs[i]["title"]
+                similarity = found_docs[i]["similarity_score"]
+                description = found_docs[i]["description"][:100]
+                print(f"{i + 1}. {title} (similarity: {similarity:.3f})")
+                print(f"   {description}...")
 
         case _:
             parser.print_help()
